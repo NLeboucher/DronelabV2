29ce12e5e2e85604aa4aba303c166bd7f1e48436
nico <nicolas.leboucher01@gmail.com>
2024-01-11 10:29:11 +0100

    updated viewer has error on dragcontrol

--- API/static/app.js
+++ API/static/app.js
@@ -1,17 +1,20 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { DragControls} from 'three/examples/DragControls.js'
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 // Drone Setup
 const IP="172.21.73.34"
 const apiUrl = `http://${IP}:8080/`;
-const useTrueDrones = true;
+const useTrueDrones = false;
 
 // Scene, Camera, Renderer Setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
+
 renderer.setSize(window.innerWidth, window.innerHeight);
+// renderer.domElement.setAttribute('draggable', 'true');
 document.body.appendChild(renderer.domElement);
 
 // Load the Background Texture
@@ -25,6 +28,17 @@ textureLoader.load('assets/equirectangular_bg.png', function (texture) {
 const light = new THREE.AmbientLight(0xffffff, 3); // soft white light
 light.position.set(0, 1, 1).normalize();
 scene.add(light);
+
+// create a wireframe box selection
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshBasicMaterial({
+    color: 0xffff00,
+    wireframe: true
+});
+const selectionBox = new THREE.Mesh(boxGeometry, boxMaterial);
+selectionBox.visible = false; // Initially hidden
+scene.add(selectionBox);
 
 // add fog
 // scene.fog = new THREE.FogExp2(0x000000, 0.6);
@@ -42,6 +56,9 @@ class Drone {
         this.position = position;
         this.model = model.clone(); // Clone the model
         this.model.position.set(position.x, position.z, position.y);
+        this.goalPosition = new THREE.Vector3(position.x, position.z, position.y);
+        this.model.castShadow = true;
+        this.model.receiveShadow = true;
         scene.add(this.model);
     }
     toString() {
@@ -75,6 +92,7 @@ const loader = new GLTFLoader();
 let droneModel; // This will hold the original loaded model
 const axesHelper = new THREE.AxesHelper( 5 );
 scene.add( axesHelper );
+let drones = [];
 loader.load('assets/drone.glb', (gltf) => {
     droneModel = gltf.scene;
     console.log(droneModel)
@@ -83,7 +101,10 @@ loader.load('assets/drone.glb', (gltf) => {
 
     // Once the model is loaded, create drones
     createDrones();
+    
+
 });
+
 async function asyncCall(call="") {
     console.log('calling'+ useTrueDrones);
     if(useTrueDrones){
@@ -106,30 +127,39 @@ async function asyncCall(call="") {
     
     // Expected output: "resolved"
   }
-let drones = [];
+
+
+
 function createDrones(N=5) {
 
     // const N = 5; // Number of drones
     for (let i = 0; i < N; i++) {
         const random_position = new THREE.Vector3(
-            Math.random() * 1 - 0.5,
-            Math.random() * 1 - 0.5,
-            Math.random() * 1 - 0.5
+            Math.random() * 3,
+            Math.random() * 3,
+            Math.random() * 3,
         );
         const drone = new Drone(i, random_position, droneModel);
-
+        // console.log(drone.goalPosition.x,drone.goalPosition.y,drone.goalPosition.z)
         drones.push(drone);
+
         let SepcubeFolder = cubeFolder.addFolder('Drone '+i)
         SepcubeFolder.add(drone, 'id');
         SepcubeFolder.add(drone, 'URI');
-        SepcubeFolder.add(drone.position, 'x');
-        SepcubeFolder.add(drone.position, 'y');
-        SepcubeFolder.add(drone.position, 'z');
+        let PositionFolder = SepcubeFolder.addFolder('Position '+i)
+
+        PositionFolder.add(drone.position, 'x');
+        PositionFolder.add(drone.position, 'y');
+        PositionFolder.add(drone.position, 'z');
+        let GoalPositionFolder = SepcubeFolder.addFolder('Destination '+i)
+        GoalPositionFolder.add(drone.goalPosition, 'x');
+        GoalPositionFolder.add(drone.goalPosition, 'y');
+        GoalPositionFolder.add(drone.goalPosition, 'z');
         // SepcubeFolder.add(drone.model.position, 'x');
         // SepcubeFolder.add(drone.model.position, 'y');
         // SepcubeFolder.add(drone.model.position, 'z');
         // SepcubeFolder.add(drone, 'model');
-        console.log(drone.toString())
+        // console.log(drone.toString())
         // const drone = new Drone(i, new THREE.Vector3(i * 0.5, 0, 0), droneModel);
 
         // const box = new THREE.BoxHelper( drone.model, 0xffff00 );
@@ -142,12 +172,11 @@ function createDrones(N=5) {
 camera.position.z = 2;
 async function updatePositions() {
     drones.forEach(item => {
-        console.log(item.toString())
+        // console.log(item.toString())
         item.model.position.set(item.position.x, item.position.z, item.position.y);
     }); 
 }
 
-var interpolationVector = new THREE.Vector3();
 
 function animateDrones() {
     drones.forEach(drone => {
@@ -155,20 +184,49 @@ function animateDrones() {
         for (let i = 5; i <= 8; i++) {
             let propeller = drone.model.children[0].children[i];
             let speed = 0.2;
-            if(drone.position.z>0.1){
+            if(drone.position.z>0.0){
                 propeller.rotation.z += speed;
+            }
+            if(drone.position.z<0.0){
+                drone.position.z=0.0;
             }
             
         }
     });
 }
 
+var alpha = 0.000;
+const alphaIncrement = 0.001; // Adjust this value for speed of interpolation
+
+function InterpolateDroneMotion() {
+    drones.forEach(drone => {
+        if (!drone.position.equals(drone.goalPosition)&&drone.position.z>0.0) {
+            // Interpolate towards the goal position
+            drone.position.lerpVectors(drone.position, drone.goalPosition, alpha);
+            // console.log("Interpolated Position", drone.id, ":", drone.position.x, drone.position.y, drone.position.z);
+        }
+    });
+
+    alpha += alphaIncrement;
+    if (alpha > 1) {
+        alpha = 0; // Reset alpha if it exceeds 1
+        // Optionally, update drone.goalPosition here if you want a continuous movement
+    }
+}
+let f = 0;
+let start=Date.now();
 // Animation Loop
 function animate() {
+    f+=1;
+    // console.log(Math.floor((Date.now()-start))/1000)
+    if(Math.floor((Date.now()-start)/1000)>=1){
+        console.log(f); 
+        f=0;start=Date.now();
+    }
     
     updatePositions();
     animateDrones();
-
+    InterpolateDroneMotion();
 
     requestAnimationFrame(animate);
     controls.update(); // Only required if controls.enableDamping or controls.autoRotate are set to true
@@ -176,4 +234,6 @@ function animate() {
 
 }
 animate();
+
+
 
