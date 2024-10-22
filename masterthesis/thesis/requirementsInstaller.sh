02d3488becd1141b20c84616b8a1a31076433454
nico <nicolas.leboucher01@gmail.com>
2024-10-22 14:50:03 +0200

    added master thesis

---
+++ masterthesis/thesis/requirementsInstaller.sh
@@ -0,0 +1,5 @@
+sudo apt-get install texlive-latex-base texlive-latex-extra texlive-latex-recommended texlive-science
+sudo apt-get install xzdec
+sudo apt-get install perl-tk
+tlmgr init-usertree
+tlmgr update --all
